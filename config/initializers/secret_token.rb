d52a7b5abda5bcb1c032e228b39f6e2e8da5aecd
Andrey Astashov <mvc.aaa@gmail.com>
2013-04-03 02:41:56 +0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MvcWww::Application.config.secret_token = '6b3f2b4fcc161948de6bb8855fb3566844c69232544cf9cf74c7e00443c125f62075f41bdb64745eb7a0dd3af2cdcc34256842816a21851e16157007e321fe3d'
